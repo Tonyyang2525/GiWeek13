286db97eb8599e1b05ed318c8c74c1ea5a393811
Tonyyang2525 <tonyyang2525@gmail.com>
2024-06-16 16:34:53 -0400

    Final commit of files with psuedo code  and updates to css(Hero Image)

--- about.js
+++ about.js
@@ -1,21 +1,36 @@
+//When user clicks a movie from the list, it will show details of the movie in a new tab
 window.onload = async (event) => {
+  //getting id from url
   var urlParams = new URLSearchParams(window.location.search);
+  //storing id in variable
+  //url params is an object
   const movieId = urlParams.get("id");
-
+  //stored variable where the results will be stored
+  //calling the api
   const resDetail = await axios.get(`http://localhost:3000/movie/${movieId}`);
 
   const resSimilar = await axios.get(
     `http://localhost:3000/similar/${movieId}`
   );
-
+  //storing results in variables
   const movieDetails = resDetail.data;
   const movieList = resSimilar.data.results;
-  console.log("movieDetails", movieDetails);
+
+  //calling functions
+  showMovieDetails(movieDetails);
+  showSimilarMovies(movieList);
+};
+async function showMovieDetails(movieDetails) {
+  //if poster path is null, show the image of no image
+  //else show the image of the poster
   if (movieDetails.poster_path === null) {
+    //if condition is true, insert my personal image from google as a no image movie
     movieDetails.poster_path = `noImage.avif`;
   } else {
     movieDetails.poster_path = `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`;
   }
+  //grabs the id from html using DOM
+  //creating elemetns with DOM inside of the id
   document.getElementById("movie-details").innerHTML = `
     <img src="${movieDetails.poster_path}" alt="Avatar" style="width: 100%" />
     <div class = "container">
@@ -23,11 +38,12 @@ window.onload = async (event) => {
     <h2>${movieDetails.tagline}</h2>
     <p>${movieDetails.overview}</p>
     </div>
-   
-
     `;
+}
 
+async function showSimilarMovies(movieList) {
   document.getElementById("similar-movies").innerHTML = "";
+  //for each movie there is in the movielist, return the properties that are choosen from the api
   for (let movie of movieList) {
     let postImage = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
     if (movie.poster_path === null) {
@@ -47,37 +63,5 @@ window.onload = async (event) => {
   </a>
 </div>
 `;
-  }
-};
-
-async function clickSearch() {
-  const movieName = document.getElementById("movie-name").value;
-
-  if (movieName.trim() === "") {
-    //this does not work, node usess process. argv
-    return alert("Please enter a movie name");
-  }
-  const response = await axios.get(`http://localhost:3000/${movieName}`);
-
-  const movieList = response.data.results;
-
-  document.getElementById("movies").innerHTML = "";
-  for (let movie of movieList) {
-    document.getElementById("movie-").innerHTML += `
-    <a href="/about.html?id=${movie.id}">
-    <div class="card">
-    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="Avatar" style="width: 100%" />
-<div class="container">
-  <h4><b>${movie.title}</b></h4>
-</div>
-</div>
-</a>`;
-    document.getElementById("movie-details").innerHTML = "";
-    for (let movie of movieList) {
-      document.getElementById("movie-details").innerHTML += `
-    <h1>${movie.title}</h1>
-    <p>${movie.overview}</p>
-    `;
-    }
   }
 }
--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 async function clickSearch() {
   const movieName = document.getElementById("movie-name").value;
-
+  // if there are no strings in the search then alert the user
+  //"please enter a movie name"
   if (movieName.trim() === "") {
     return alert("Please enter a movie name");
   }
+  //calling the api
   const response = await axios.get(`http://localhost:3000/${movieName}`);
 
   const movieList = response.data.results;
 
   document.getElementById("movies").innerHTML = "";
+  //for each movie there is in the movielist, return the properties that are choosen from the api
   for (let movie of movieList) {
     let postImage = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
     if (movie.poster_path === null) {
@@ -26,6 +29,8 @@ async function clickSearch() {
   </a>
 </div>`;
   }
-
+  //this targets my id of "movie-search-container"
+  //this will make it display none at the end of the search so that insed of the get stacking on each other
+  //it can be replaced with another id
   document.getElementById("movie-search-container").style.display = "none";
 }
